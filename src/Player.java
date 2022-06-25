9a34c4446ac082a5736e3c9f480853952df0919e
maevskiEd <maevski.ed@gmail.com>
2022-06-25 23:23:00 +0300

    for work

---
+++ src/Player.java
@@ -0,0 +1,16 @@
+public class Player extends Human {
+    Backpack backpack = new Backpack();
+
+    class Backpack{
+        String things="";
+
+    }
+
+    public Player(String name) {
+        super(name);
+    }
+
+    public void take(String thing){
+
+    }
+}
